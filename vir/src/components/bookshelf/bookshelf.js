87856e0f67d22aa3e72dc47edd81de4b68b9d604
Arthur Zanella Lovato <arthur.zanella.lovato.1998@gmail.com>
2024-09-08 19:39:51 -0300

    Criação do componente bookshelf

---
+++ vir/src/components/bookshelf/bookshelf.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './bookshelf.css';
+import accountCircle from './account_circle.png';
+import SideMenu from '../side_menu/side_menu';
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const handleBackClick = () => {
+    window.history.back();
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
+  return (
+    <header className="header">
+      <button className="back-button" onClick={handleBackClick}>
+        Voltar
+      </button>
+      <h1 className="header-title">MEUS LIVROS</h1>
+      <img 
+        src={accountCircle} 
+        className="profile-icon" 
+        onClick={toggleMenu}
+        alt="Ícone de perfil" 
+      />
+      
+      {isMenuOpen && <SideMenu isOpen={isMenuOpen} toggleMenu={toggleMenu} menuRef={menuRef} />}
+    </header>
+  );
+};
+
+export default Header;
