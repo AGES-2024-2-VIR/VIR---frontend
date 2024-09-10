b49b675b69757b7b307f400b0c5d0e77ac462aa7
Pedro Fonseca <pedrofonseca447@gmail.com>
2024-09-10 18:34:36 -0300

    preparando para dar merge

--- vir/src/App.js
+++ vir/src/App.js
@@ -1,29 +1,18 @@
 import './App.css';
 import React from 'react';
 import Header from './components/bookshelf/bookshelf';
-import BookReader from './components/reader/bookreader';
-import Navbar from './components/footer-component/footer';
-import WithLabelExample from './components/progress-bar/progress';
 
 
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />  
       <main>
-         
-        <WithLabelExample />
-          <Navbar></Navbar>
-
-          
+        
       </main>
     </div>
   );
-
-
-
-  
 }
 
 export default App;
